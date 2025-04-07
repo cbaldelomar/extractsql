5d339d7ed97becbbefe7e712255b4c3115c78047
balde <cbaldelomar@hotmail.com>
2025-04-07 11:37:09 -0600

    Add "Intended Audience" classifier to setup.py

--- setup.py
+++ setup.py
@@ -46,5 +46,6 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
     ],
 )
